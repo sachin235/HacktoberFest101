461866b9aa8dbea2eb14c8a6fa9f33195f35624e
Damon <dam286@pitt.edu>
2021-10-28 19:59:04 -0400

    Create TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+
+// target = 10;
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int k = i+1; k < nums.length; k++) {
+                if (nums[j] == compliment)
+                    return new int [] {i, j};
+            }
+        }
+    }
+}
+// @lc code=end
