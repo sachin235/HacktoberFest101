81d15e48e29c8dc7924086ede1007c0f59ba4b09
sachin235 <32926581+sachin235@users.noreply.github.com>
2022-10-30 22:25:09 +0530

    Update mersenneprime.java

--- mersenneprime.java
+++ mersenneprime.java
@@ -1,4 +1,4 @@
-//To find mersenne's prime upto 
+// A Mersenne prime is a prime number that is one less than a power of two. That is, it is a prime number of the form Mn = 2n − 1 for some integer n. 
 
 import java.util.*;
 import java.lang.*;
