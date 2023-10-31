17c2d3a422b77db885274d9295bc1f0350971c78
ujjwalmittal04 <48005236+ujjwalmittal04@users.noreply.github.com>
2023-10-31 13:46:25 +0530

    fix issue

--- C++/Dynamic Programming/Triangle.cpp
+++ C++/Dynamic Programming/Triangle.cpp
@@ -1,3 +1,7 @@
+//Given a triangle array, return the minimum path sum from top to bottom.
+// For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
